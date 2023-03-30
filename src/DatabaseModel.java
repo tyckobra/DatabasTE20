f8825f46999e37fc5b0d587b84dad8fccf018dd5
tyckobra <77719515+tyckobra@users.noreply.github.com>
2023-03-30 13:40:43 +0200

    Börjat på login som user

--- src/DatabaseModel.java
+++ src/DatabaseModel.java
@@ -9,27 +9,38 @@ public class DatabaseModel {
         DatabaseModel m;
         DatabaseView v;
 
-       JPasswordField pf = new JPasswordField();
-       JOptionPane.showConfirmDialog(null, pf, "password?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
-       String password = new String(pf.getPassword());
+        JPasswordField pf = new JPasswordField();
+        JOptionPane.showConfirmDialog(null, pf, "password?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        String password = new String(pf.getPassword());
 
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://db.umea-ntig.se:3306/te20? "+
+            conn = DriverManager.getConnection("jdbc:mysql://db.umea-ntig.se:3306/te20? " +
                     "allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC", user, password);
         } catch (SQLException e) {
             e.printStackTrace();
         }
 
+        //LOGGA IN MED ANVÄNDARE
+
+        String SQLQuery;
+        SQLQuery = "SELECT * FROM tb02users";
+        String User = new String();
+        String UserPassword = new String();
+        JOptionPane.showConfirmDialog(null, User, "Username?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showConfirmDialog(null, UserPassword, "Password?", JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
 
         try {
+            if (User == "") UserPassword = "";{ return("Enter Username and Passworn");}
+                else if ()
+            }
             Statement stmt = conn.createStatement();
-            String SQLQuery = "SELECT * FROM tb02forum";
+            SQLQuery = "SELECT * FROM tb02forum";
             ResultSet result = stmt.executeQuery(SQLQuery);
 
             ResultSetMetaData metadata = result.getMetaData();
 
             int numCols = metadata.getColumnCount();
-            for (int i = 1 ; i <= numCols ; i++) {
+            for (int i = 1; i <= numCols; i++) {
                 System.out.println(metadata.getColumnClassName(i));
             }
 
